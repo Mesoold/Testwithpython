a9281e22462c460f2c9454cdd1f77850f38b50ec
Mesoold <104485900+Mesoold@users.noreply.github.com>
2022-05-31 17:33:05 +0700

    Update python-1.py
    ตอนที่เราใส่ temperature unit = x มันไม่ออกทันทีแต่มันถามหา temperature = ต่อเลยแก้ให้ถ้าพิมพ์ x จะออกทันที

--- python-1.py
+++ python-1.py
@@ -7,34 +7,46 @@ while (converter == True):
     #ค่าอุณหภูมิที่ทั้งเซลเซียส,ฟาไรนไฮต์,เคลวิน,โรเมอร์ โดยใส่เพียงตัวย่อของแต่ละอุณหภูมิ จำเป็นต้องใส่ทั้งหน่วยของอุณหภูมิที่ต้องการจะะหา และค่าของอุณหภูมินั้นๆ
     print("degree Celsius : C, degree Fahrenheit : F, Kelvin : K, Réaumur : R, Exit : X")
     Tem_unit = str(input("temperature unit = "))
-    Temper = float(input("temperature = "))
-    print("------------------------------------------------------")
-    if Tem_unit == 'C' or Tem_unit == 'c':
+    if Tem_unit == 'x' or Tem_unit == 'X':
+        converter = False
+        print("EXIT ....") 
+        exit(0)
+    
+    elif Tem_unit == 'C' or Tem_unit == 'c':
+        Temper = float(input("temperature = "))
+        print("------------------------------------------------------")
         f = (Temper*9/5)+32
         k = Temper+273.15
         r = Temper*0.8
         print("your degree Celsius is {:.2f}\ndegree Fahrenheit is {:.2f}\nKelvin is {:.2f}\nRéaumur is {:.2f}".format(Temper, f, k, r))
     elif Tem_unit == 'F' or Tem_unit == 'f':
+        Temper = float(input("temperature = "))
+        print("------------------------------------------------------")
         c = 5/9*(Temper-32)
         k = c+273.15
         r = c*0.8
         print("your degree Fahrenheit is {:.2f}\ndegree Celsius is {:.2f}\nKelvin is {:.2f}\nRéaumur is {:.2f}".format(Temper, c, k, r))
     elif Tem_unit == 'K' or Tem_unit == 'k':
+        Temper = float(input("temperature = "))
+        print("------------------------------------------------------")
         c = Temper-273.15
         f = (c*9/5)+32
         r = c*0.8
         print("your Kelvin is {:.2f}\ndegree Celsius is {:.2f}\ndegree Fahrenheit is {:.2f}\nRéaumur is {:.2f}".format(Temper, c, f, r))
     elif Tem_unit == 'R' or Tem_unit == 'r':
+        Temper = float(input("temperature = "))
+        print("------------------------------------------------------")
         c = Temper*1.25
         f = (c*9/5)+32
         k = c+273.15
         print("your Réaumur is {:.2f}\ndegree Celsius is {:.2f}\ndegree Fahrenheit is {:.2f}\nKelvin is {:.2f}".format(Temper, c, f, k))
     elif Tem_unit == 'x' or Tem_unit == 'X':
+        Temper = float(input("temperature = "))
+        print("------------------------------------------------------")
         converter = False
         print("EXIT ....") 
         exit(0)
     else :
         print("----------Please enter temperature unit again....---------") 
         continue
-
 
